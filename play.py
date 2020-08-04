4716894f1ff501423736be161dfaa63ff626664e
William Horst <you@example.com>
2020-08-03 23:40:21 -0400

    add ui component

---
+++ play.py
@@ -0,0 +1,9 @@
+from RedditScraperService.paperTrading import paperTradingUtilities
+
+if __name__ == "__main__":
+    api = paperTradingUtilities.getRestApiInterface()
+    # print(api.get_account())
+    # print(api.get_account().__getattr__("portfolio_value"))
+    print(api.get_calendar())
+    # print(api.list_positions())
+    # print(api.get_portfolio_history(date_start="2020-07-06", date_end="2020-07-10", timeframe="1D"))
